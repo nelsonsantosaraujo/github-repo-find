fbf3b6b24313e0d23c7324dc4cd00166bb7920db
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-02-15 22:30:46 -0300

    feat: Create initial structure of repository component

--- src/pages/repository/index.tsx
+++ src/pages/repository/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import { Container, RepositoryInfo} from './style';
+
+import Logo from '../../img/logo-github.svg';
+
 const Repository: React.FC = () => {
   return (
-    <div>
-      <h1>Repositories</h1>
-    </div>
+    <>
+      <img src={Logo} alt="Logo App" />
+      <Container>
+        <img src="https://via.placeholder.com/384" alt="avatar"/>
+        <RepositoryInfo>
+          <h1>Title Repo</h1>
+          <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Si num tem leite então bota uma pinga aí cumpadi! Delegadis gente finis, bibendum egestas augue arcu ut est. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Manduma pindureta quium dia nois paga.
+          </p>
+        </RepositoryInfo>
+      </Container>
+    </>
   );
 }
 
---
+++ src/pages/repository/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 40px;
+  display: flex;
+
+  img {
+    border-radius: 50%;
+  }
+
+`;
+
+export const RepositoryInfo = styled.div`
+  margin-left: 80px;
+
+  h1 {
+    font-size: 68px;
+    font-weight: bold;
+    margin-bottom: 24px;
+  }
+
+  p {
+    font-size: 18px;
+  }
+`;
