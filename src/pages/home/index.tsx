4270b8e1bb5e3a7bad7520ac270780f1cc965214
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-02-15 22:29:26 -0300

    chore: add Link component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FormEvent } from 'react';
+import { Link } from 'react-router-dom'
 import { FiChevronRight } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -63,14 +64,14 @@ const Home: React.FC = () => {
       </Form>
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link key={repository.full_name} to="/repo">
             <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
             <div>
               <strong>{repository.owner.login}</strong>
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={40} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
