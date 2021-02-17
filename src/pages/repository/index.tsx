4afca4e0f553b01ea671afa905d4d973f40d4f3b
Nelson Araújo <nelsonsantosaraujo@hotmail.com>
2021-02-16 23:27:29 -0300

    feat: repository component structure

--- src/pages/repository/index.tsx
+++ src/pages/repository/index.tsx
@@ -1,22 +1,138 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-import { Container, RepositoryInfo} from './style';
+import { Header, RepositoryInfo, Issues} from './style';
 
 import Logo from '../../img/logo-github.svg';
+import api from '../../services/api';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+interface IRepository {
+  full_name: string;
+  description: string;
+  forks_count: number;
+  stargazers_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+
+  }
+
+}
 
 const Repository: React.FC = () => {
+  const [repository, setRepository] = useState<IRepository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect( () => {
+    api.get(`repos/${params.repository}`).then(
+      response => {
+        setRepository(response.data);
+      }
+    );
+
+    api.get(`repos/${params.repository}/issues`).then(
+      response => {
+        setIssues(response.data);
+      }
+    );
+
+    // async function loadData(): Promise<void> {
+    //   const repositoryResponse = await api.get(`repos/${params.repository}`);
+    //   const issuesResponse = await api.get(`repos/${params.repository}/issues`)
+
+    //   console.log('repository: ', repositoryResponse.data);
+    //   console.log('issues: ', issuesResponse.data);
+    // }
+
+    // loadData();
+
+
+    // async function loadData(): Promise<void> {
+    //   const [repository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`)
+    //   ])
+
+
+    //   console.log('repository: ', repository.data);
+    //   console.log('issues: ', issues.data);
+    // }
+
+    // loadData();
+
+
+  },[params.repository]);
+
   return (
     <>
-      <img src={Logo} alt="Logo App" />
-      <Container>
-        <img src="https://via.placeholder.com/384" alt="avatar"/>
+      <Header>
+        <img src={Logo} alt="Logo App" />
+        <Link to="/">
+          <FiChevronLeft size={20} />
+          Voltar
+        </Link>
+      </Header>
+      {repository && (
         <RepositoryInfo>
-          <h1>Title Repo</h1>
-          <p>
-            Mussum Ipsum, cacilds vidis litro abertis. Si num tem leite então bota uma pinga aí cumpadi! Delegadis gente finis, bibendum egestas augue arcu ut est. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Manduma pindureta quium dia nois paga.
-          </p>
+          <header>
+            <img 
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>
+                {repository.description}
+              </p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues</span>
+            </li>
+          </ul>
         </RepositoryInfo>
-      </Container>
+      )}
+
+      <Issues>
+        {issues.map(issue => (
+            <a key={issue.id} href={issue.html_url} target="_blank" rel="noreferrer noopener">
+              <div>
+                <strong>{issue.title}</strong>
+                <p>{issue.user.login}</p>
+              </div>
+              <FiChevronRight size={40} />
+            </a>
+        ))}
+      </Issues>
+
+
     </>
   );
 }
--- src/pages/repository/style.ts
+++ src/pages/repository/style.ts
@@ -1,25 +1,121 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  margin-top: 40px;
+export const Header = styled.header`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-  img {
-    border-radius: 50%;
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #A8A8B3;
+    transition: color 0.2s;
+
+    &:hover {
+      color: #666;
+    }
+
+    svg {
+      margin-right: 4px;
+    }
   }
-
 `;
 
-export const RepositoryInfo = styled.div`
-  margin-left: 80px;
+export const RepositoryInfo = styled.section`
+  margin-top: 80px;
 
-  h1 {
-    font-size: 68px;
-    font-weight: bold;
-    margin-bottom: 24px;
+  header {
+    display: flex;
+    align-items: center;
+
+    img {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    }
+
+    div {
+      margin-left: 24px;
+
+      strong {
+        font-size: 36px;
+        color: #B4B4B4;
+      }
+
+      p {
+        font-size: 18px;
+        color: #929292;
+        margin-top: 4px;
+      }
+    }
+
   }
 
-  p {
-    font-size: 18px;
+  ul {
+    display: flex;
+    list-style: none;
+    margin-top: 40px;
+
+    li {
+      & + li {
+        margin-left: 80px;
+      }
+
+      strong {
+        display: block;
+        font-size: 36px;
+        font-weight: bold;
+        color: #797777;
+      }
+
+      span {
+        display: block;
+        margin-top: 4px;
+        color: #949494;
+      }
+    }
+  }
+`;
+
+export const Issues = styled.section`
+  margin-top: 80px;
+  
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: flex;
+    text-decoration: none;
+    align-items: center;
+    transition: 0.5s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    &+a {
+      margin-top: 18px;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3D3D4D;
+      }
+
+      p {
+        font-size: 18px;
+        color: #CBCBD6;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #CBCBD6;
+    }
   }
 `;
